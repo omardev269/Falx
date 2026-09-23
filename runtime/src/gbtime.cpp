d2e42727cf073ff440655883c9f3199370fc7bb1
omardev269 <omarmismail269@gmail.com>
2026-09-23 21:57:39 +0300

    tiny time stuff update

--- runtime/src/gbtime.cpp
+++ runtime/src/gbtime.cpp
@@ -5,11 +5,16 @@
 #ifndef FLX_WIN32
 #include <gb.h>
 namespace falx {
-	void InitializeTime() {
-		
+	largefloat g_Initial;
+	void InitializeTimeInt() {
+		g_Initial = gb_time_now();
 	}
 	ulargeint GetTime() {
-		return static_cast<ulargeint>(gb_time_now() * 1000.0f);
+		return static_cast<ulargeint>((g_Initial - gb_time_now()) * 1000.0f);
+	}
+	void SleepMs(uint32 aMilliseconds) {
+		largefloat m_Target = gb_time_now() + static_cast<largefloat>(aMilliseconds) / 1000.0f;
+		while (gb_time_now() <= m_Target);
 	}
 }
 #endif // !FLX_WIN32
--- runtime/src/w32time.cpp
+++ runtime/src/w32time.cpp
@@ -8,17 +8,22 @@
 #include <Windows.h>
 namespace falx {
 	LARGE_INTEGER g_Frequency;
-	void InitializeTime() {
+	LARGE_INTEGER g_Initial;
+	void InitializeTimeInt() {
 		QueryPerformanceFrequency(&g_Frequency);
+		QueryPerformanceCounter(&g_Initial);
 	};
 	ulargeint GetTime() {
 		LARGE_INTEGER m_CurrentTime;
 		QueryPerformanceCounter(&m_CurrentTime);
 #ifdef FLX_X64
-		return ((m_CurrentTime.QuadPart * 1000) / g_Frequency.QuadPart);
+		return (((m_CurrentTime.QuadPart - g_Initial.QuadPart) * 1000) / g_Frequency.QuadPart);
 #else
-		return static_cast<ulargeint>((m_CurrentTime.QuadPart * 1000) / g_Frequency.QuadPart);
+		return static_cast<ulargeint>(((m_CurrentTime.QuadPart - g_Initial.QuadPart) * 1000) / g_Frequency.QuadPart);
 #endif // FLX_X64
+	}
+	void SleepMs(uint32 aMilliseconds) {
+		Sleep(aMilliseconds);
 	}
 }
 #endif // FLX_WIN32
