4a9caa966531ebc2c943ae10dd7ad8c054eeddd9
omardev269 <omarmismail269@gmail.com>
2026-09-25 22:56:28 +0300

    another reorganization

--- runtime/src/schedule.cpp
+++ runtime/src/schedule.cpp
@@ -12,24 +12,35 @@ namespace falx {
 }
 
 #pragma region Subsystems
+static FLX_INLINEFUNC void InitWindowSubsystem() {
+	using namespace falx;
+	CreateWindow(g_iWindow, "AlhamdullIllah!", { 800, 600 });
+	FLX_CHECK(g_iWindow != nullptr, "Window creation failed");
+}
+static FLX_INLINEFUNC void ShutdownWindowSubsystem() {
+	using namespace falx;
+	g_iWindow->Dismiss();
+	delete g_iWindow;
+}
 #pragma endregion
 
 static FLX_INLINEFUNC void StartSubsystems() {
 	using namespace falx;
 	InitializeTimeInt();
 	InitWindowSystem();
+	InitWindowSubsystem();
 }
 
 static FLX_INLINEFUNC void ShutdownSubsystems() {
 	using namespace falx;
+	ShutdownWindowSubsystem();
 	ShutdownWindowSystem();
 }
 
 void FalxStart() {
 	using namespace falx;
 	StartSubsystems();
-	CreateWindow(g_iWindow, "AlhamdullIllah!", { 800, 600 });
-	FLX_CHECK(g_iWindow != nullptr, "Window creation failed");
+
 }
 bool FalxUpdate() {
 	using namespace falx;
@@ -40,6 +51,5 @@ bool FalxUpdate() {
 }
 void FalxStop() {
 	using namespace falx;
-	delete g_iWindow;
 	ShutdownSubsystems();
 }
